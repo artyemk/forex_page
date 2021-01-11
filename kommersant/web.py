ab99b82fcfb2d01523de9afd76513cd1942b7b98
Your Name <you@example.com>
2021-01-12 02:24:55 +0500

    pizzda_rt

---
+++ kommersant/web.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, session, jsonify, make_response
+import uuid
+
+app = Flask(__name__,static_url_path='',static_folder='')
+app.secret_key = 'the random string'
+
+@app.route("/wolframinvest_article")
+def home():
+    return render_template('kommers.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5102)
