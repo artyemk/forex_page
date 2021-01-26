101e2d4b5d7c9c15acd05d0877cc22eda47f7ba1
Your Name <you@example.com>
2021-01-26 12:34:34 +0500

    pizzda_btc_v1

--- web6.py
+++ web6.py
@@ -41,4 +41,4 @@ def alter_comment():
     return redirect("/#addcomment")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5004)
+    app.run(debug=True, host="0.0.0.0", port=5016)
