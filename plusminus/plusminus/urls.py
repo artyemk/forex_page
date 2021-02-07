b9c1a4f6735df608f5cb40efb007e31476a08b0e
Your Name <you@example.com>
2021-02-07 13:47:09 +0500

    pizzda_lol

--- plusminus/plusminus/urls.py
+++ plusminus/plusminus/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from comments import views
 
 urlpatterns = [
+    path('', lambda req: redirect('/comments/')),
     path('admin/', admin.site.urls),
     path('comments/', include('comments.urls')),
     path('add_review/', views.get_name),
